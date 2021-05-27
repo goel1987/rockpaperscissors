67e6699e27449264bb267f23c7cbc2315369f08f
Gabe Levit <levitgn@mail.uc.edu>
2021-05-26 22:51:25 -0400

    added cpu response function

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -14,8 +14,8 @@ class RockPaperScissors {
    */
   generateCPUResponse(){
     const acceptedValues = [ `rock`, `paper`, `scissors` ];
-
-    return ;
+    
+    return acceptedValues[Math.floor(Math.random() * acceptedValues.length)];
   }
   /**
    * returns one of the following values: `win`, `lose`, `tie`
@@ -33,7 +33,7 @@ class RockPaperScissors {
    * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   determineWinner(userSelection, cpuSelection){
-
+    
   }
 
   /**
